1960b218862fa981cf732cc629019c8a3fa7483e
Hài hước thay <namnl.2510757@usth.edu.vn>
2026-07-30 11:54:42 +0700

    waoooo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+  // dark/light mode
+
+  const btn = document.querySelector(".btn-toggle");
+  const currentTheme = localStorage.getItem("theme");
+
+  // nếu chế độ được lưu trong localStorage là "dark"
+  if (currentTheme == "dark") {
+    // thì sẽ dùng .dark-theme class
+    document.body.classList.add("dark-theme");
+  }
+
+  btn.addEventListener("click", function() {
+    // Toggle class .dark-theme mỗi lần click
+    document.body.classList.toggle("dark-theme");
+    
+    let theme = "light";
+    // Nếu body chứa class .dark-theme
+    if (document.body.classList.contains("dark-theme")) {
+      // thì gán biến theme thành dark
+      theme = "dark";
+    }
+    // lưu lựa chọn vào localStorage
+    localStorage.setItem("theme", theme);
+  });  
+  
   // nav
 
       const navBar = document.querySelector("header nav");
